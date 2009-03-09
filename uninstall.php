0976db5c60829244fef2308b882a7399b6182db8
textwise <textwise@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-09 20:17:43 +0000

    First upload of current files
    git-svn-id: https://plugins.svn.wordpress.org/textwise/trunk@100791 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,48 @@
+<?php
+
+//Safety checks:
+//Cannot execute by direct path, only frop WP
+if( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+	die('Cannot run directly');
+
+//Cannot execute unless you have proper privs
+if ( ! current_user_can('delete_plugins') )
+	wp_die(__('You do not have sufficient permissions to delete plugins for this blog.'));
+
+//All clear:
+
+//Delete options
+delete_option('textwise_tag_enable');
+delete_option('textwise_contentlink_enable');
+delete_option('textwise_category_enable');
+delete_option('textwise_video_enable');
+delete_option('textwise_image_enable');
+delete_option('textwise_rss_enable');
+delete_option('textwise_wiki_enable');
+delete_option('textwise_product_enable');
+delete_option('textwise_autoupdate');
+delete_option('textwise_logo_place');
+delete_option('textwise_use_logo');
+delete_option('textwise_conflict_warning');
+delete_option('textwise_box_position');
+delete_option('textwise_list_css');
+
+//Delete post meta
+$metakeys[] = '_tw_tag_list';
+$metakeys[] = '_tw_cat_list';
+$metakeys[] = '_tw_video_list';
+$metakeys[] = '_tw_image_list';
+$metakeys[] = '_tw_rss_list';
+$metakeys[] = '_tw_wiki_list';
+$metakeys[] = '_tw_product_list';
+
+$allposts = get_posts('numberposts=-1&post_type=post&post_status=');
+
+foreach( $allposts as $postinfo) {
+	foreach( $metakeys as $meta) {
+		delete_post_meta($postinfo->ID, $meta);
+	}
+}
+
+
+?>
